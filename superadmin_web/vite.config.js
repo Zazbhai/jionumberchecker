36243275940213f4cc0e230ca7fbc8ee87ea47d9
Zaz Yagmi <96649732+Zazbhai@users.noreply.github.com>
2026-05-26 23:29:17 +0530

    commit

--- superadmin_web/vite.config.js
+++ superadmin_web/vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: '0.0.0.0',
+    allowedHosts: ['jio.vishotp.shop'],
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
